03af0982241531a78fc022fd1f8bcfaa0682ff21
codyuhi <codylkuhi@gmail.com>
2019-07-19 16:02:17 -0400

    Update cover.js

--- cover.js
+++ cover.js
@@ -1,1 +1,16 @@
-//Nothing for now
+//Detect mobile device
+function mobileRedirect(){
+    var uagent = navigator.userAgent.toLowerCase();
+    if(uagent.search("iphone") > -1 
+        || uagent.search("ipad") > -1
+        || uagent.search("android") > -1
+        || uagent.search("blackberry") > -1
+        || uagent.search("webos") > -1){
+            console.log("Mobile device detected");
+            alert("Mobile device detected");
+            $(".catchphrase").attr("style","font-size:1.5vw;");
+            //window.location.href = "m_index.html";
+        }else{
+            console.log("Desktop device detected");
+        }
+}
