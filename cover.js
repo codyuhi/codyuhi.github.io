ebc21672be8da0036c3c8dbcda792d0e47ee4500
codyuhi <codylkuhi@gmail.com>
2019-07-19 16:10:10 -0400

    Update cover.js

--- cover.js
+++ cover.js
@@ -7,7 +7,7 @@ function mobileRedirect(){
         || uagent.search("blackberry") > -1
         || uagent.search("webos") > -1){
             console.log("Mobile device detected");
-            alert("Mobile device detected");
+            //alert("Mobile device detected");
             $(".catchphrase").attr("style","font-size:1.5vw;");
             //window.location.href = "m_index.html";
         }else{
